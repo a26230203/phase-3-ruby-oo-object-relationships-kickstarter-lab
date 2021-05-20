fa0e91fe0b967f483ee1b9c01e6d5d5ffc10328d
Bin <binlin35@yahoo.com>
2021-05-20 14:30:14 -0400

    Completed assignment

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+
+class Project
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        ProjectBacker.all.select do |backer_instance|
+            backer_instance.project == self
+        end.map { |backer_instance| backer_instance.backer }
+    end 
+end 
